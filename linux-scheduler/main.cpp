613e4a0e835328b880a35ddb6c49df1e758db8ae
Cristopher <cristopherpk@gmail.com>
2015-09-30 20:35:43 -0700

    Initial Commit

---
+++ linux-scheduler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  linux-scheduler
+//
+//  Created by Cristopher Freitas on 9/30/15.
+//  Copyright © 2015 Cristopher Freitas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
